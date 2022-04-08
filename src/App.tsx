cf4a552eb072aa69b8a27d44fad53ec4c42f4733
cherry15 <cherylanncolier@gmail.com>
2022-04-08 10:42:00 +0100

    menu interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import About from './pages/about'
 import './App.css'
+import About from './pages/about'
 import Home from './pages/home'
+import Cats from './pages/cats'
+import Dogs from './pages/dogs'
+import Contact from './pages/contact'
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
      <Routes>
        <Route path="/" element={<Home />} />
        <Route path="/about" element={<About />} />
+       <Route path="/cats" element={<Cats />} />
+       <Route path="/dogs" element={<Dogs />} />
+       <Route path="/contact" element={<Contact />} />
      </Routes>
     </div>
   )
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import './header.css'
 
 const Header = () => {
+  const openMenuLabel = 'Open menu'
+  const closeMenuLabel = 'Close menu'
+  const [ariaMenuLabel, setAriaMenuLabel] = useState(openMenuLabel)
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
+    isMenuOpen
+      ? setAriaMenuLabel(closeMenuLabel)
+      : setAriaMenuLabel(openMenuLabel)
+  }
+
+  const hideMenu = () => {
+    setIsMenuOpen(false)
+  }
+
   return (
     <header className="header">
       <div>Logo</div>
-      <input type="button" className='open' />
-      <ul className="menu">
-        <li><a className='active'>One</a></li>
-        <li><a>Two</a></li>
-        <li><a>Three</a></li>
-        <li><a>Four</a></li>
+      <input
+        type="button"
+        className={`menuButton ${isMenuOpen ? 'open' : ''}`}
+        aria-label={ariaMenuLabel}
+        onClick={toggleMenu}
+      />
+      <ul className={`menu ${isMenuOpen ? 'open' : ''}`}>
+        <li>
+          <NavLink to={'/about'} className={({ isActive }) => (isActive ? 'active' : '')} onClick={hideMenu} >About</NavLink>
+        </li>
+        <li>
+        <NavLink to={'/cats'} className={({ isActive }) => (isActive ? 'active' : '')} onClick={hideMenu} >Cats</NavLink>
+        </li>
+        <li>
+        <NavLink to={'/dogs'} className={({ isActive }) => (isActive ? 'active' : '')} onClick={hideMenu} >Dogs</NavLink>
+        </li>
+        <li>
+        <NavLink to={'/contact'} className={({ isActive }) => (isActive ? 'active' : '')} onClick={hideMenu} >Contact</NavLink>
+        </li>
       </ul>
     </header>
   )
