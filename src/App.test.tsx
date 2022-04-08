7ae4b2715978996bd5bb30582b6efbc94f14d026
cherry15 <cherylanncolier@gmail.com>
2022-04-08 12:01:26 +0100

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { MemoryRouter } from 'react-router-dom'
+// import { renderWithRouter } from './test/test-utils'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('app', () => {
+  render(<App />, { wrapper: MemoryRouter })
+  expect(screen.getByText(/home/i)).toBeInTheDocument()
+})
+
+// test('full app rendering/navigating', async () => {
+//   const {user}: any = renderWithRouter(<App />)
+//   expect(screen.getByText(/home/i)).toBeInTheDocument()
+//   await user.click(screen.getByText(/about/i))
+//   expect(screen.getByText(/about/i)).toBeInTheDocument()
+// })
